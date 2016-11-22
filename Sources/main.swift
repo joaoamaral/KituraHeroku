232f8f383b8af2326d0bdd466c5d39b340d3e0af
João Amaral <jamaral@pasonatquila.com>
2016-11-22 13:45:10 +0900

    added Procfile

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,17 +1,18 @@
 import Kitura
 import HeliumLogger
 import Foundation
-import SwiftyJSON
 
 HeliumLogger.use()
 
 let router = Router()
 
-router.get("/"){
-  request, response, next in
-    response.send("Hello, World!")
-    next()
+router.get("/") {
+   request, response, next in
+   response.send("Hello, World!")
+   next()
 }
-let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8091") ?? 8091
+
+let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090
+
 Kitura.addHTTPServer(onPort: port, with: router)
 Kitura.run()
